7a26be19c6e2eb2442eb590b7ac33d6e59980b8b
Bintang Ilham S <36228385+BintangIlhamSy@users.noreply.github.com>
2020-10-22 15:22:06 +0700

    Update config.php

--- simple-php-website-master/includes/config.php
+++ simple-php-website-master/includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Portofolio Bintang Ilham Syahputra',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
